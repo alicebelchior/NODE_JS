b162541ddff60c605a6fef896b027203cc59f9ab
Alice Belchior <alice.belchior23@gmail.com>
2024-01-04 14:04:52 -0300

    aula 15
    Promise

---
+++ aula15/index.js
@@ -0,0 +1,30 @@
+/*
+A "PROMISE" é uma evolução das "callbacks"
+ - Callback é uma função passada como parâmetro para outra função e quando essa outra função termina de executar o que ela tinha que fazer,
+ ela chama o callback e ele executa o que está dentro dele
+
+ - A Promise é uma promessa, ou seja, ou ela pode ser cumprida ou não (rejeitada).
+*/
+function cadastrarUsuario(){
+    return new Promise((resolve, reject) =>{
+
+        setTimeout(() =>{
+          //logica para cadastrar o usuario
+          
+          var erro = false;  //false/true (teste)
+          if (!erro) {
+            resolve("Sucesso");
+          } else {
+            reject("Ocorreu um erro")
+          }
+        }, 2500)
+    });
+}
+
+cadastrarUsuario()
+.then((mensagem) =>{ //resolve
+    console.log(mensagem);
+})
+.catch((mensagem) =>{ //reject
+    console.log(mensagem)
+})
